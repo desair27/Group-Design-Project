f848c55b693c18d859baf719d9fdb9692cc3113e
Kian Fay <kifay@tcd.ie>
2022-02-16 14:12:32 +0000

    Initial commit

--- CityExplorer/App.js
+++ CityExplorer/App.js
@@ -1,4 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+    {/*Render our MapView*/}
+      <MapView
+        style={styles.map}
+        //specify our coordinates.
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
+}
+//create our styling code:
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1, //the container will fill the whole screen.
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+
+/* import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import React from 'react';
@@ -14,4 +49,4 @@ const YourApp = () => {
   );
 }
 
-export default YourApp;
+export default YourApp; */
