17ebdd801621e2b20acbc7a6dac7d58dd7c1791b
Tom Tye <tyet@tcd.ie>
2022-02-24 21:50:39 +0000

    Location Services
    Added in some code from the react-native-location community project to prompt the user for access to their location, and then feed it into the react-native-maps code.
    -updated android manifest
    -updated HomeScreen.js
    
    Having issues linking the library with the codebase
    -'circular dependency' error

--- app/src/views/screens/HomeScreen.js
+++ app/src/views/screens/HomeScreen.js
@@ -3,11 +3,31 @@ import React, { useState, useEffect } from 'react';
 
 import { View } from 'react-native';
 import MapView from 'react-native-maps';
-// import Geolocation from '@react-native-community/geolocation';
+import RNLocation from "react-native-location";
 
 import styles from '../../styles';
 
 const HomeScreen = () => {
+
+    RNLocation.configure({
+        distanceFilter: 5.0
+    })
+
+    RNLocation.requestPermission({
+        android:{
+            detail: "coarse",
+            rationale: {
+                title: "Allow location access?",
+                message: "CityExplorer uses your location to track your exploration progress. Please press Allow.",
+                buttonPositive: "Allow",
+                buttonNegative: "Do not allow"
+            }
+        }
+    }).then(granted => {
+        if (granted) {
+            this._startUpdatingLocation();
+        }
+    })
     // const [position, setPosition] = useState({
     //     latitude: 10,
     //     longitude: 10,
@@ -29,14 +49,29 @@ const HomeScreen = () => {
     //     });
     // }, []);
 
+    
+    _startUpdatingLocation = () => {
+        this.locationSubscription = RNLocation.subscribeToLocationUpdates(
+            locations => {
+                this.setState({ location: locations[0] });
+
+        })
+    }
+
     return (
+        
         <View style={styles.container}>
+            const { location } = this.state;
             <MapView
                 style={styles.map}
+                
                 // initialRegion={position}
                 initialRegion={{
-                    latitude: 53.34366745,
-                    longitude: -6.254444724511822,
+
+                    latitude: location.latitude,
+                    longitude: location.longitude,
+                    //latitude: 53.34366745,
+                    //longitude: -6.254444724511822,
                     latitudeDelta: 0.0922,
                     longitudeDelta: 0.0421,
                 }}
